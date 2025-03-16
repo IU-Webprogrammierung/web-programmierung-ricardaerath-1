73855179370c2b10654dc81d259ca6f9060778fc
RicardaErath <ricarda.erath@iubh-fernstudium.de>
2025-03-16 22:51:20 +0100

    added close icon in menu

--- navigation.js
+++ navigation.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.querySelector(".hamburger");
+    const bars = document.querySelector(".bars");
+    const close = document.querySelector(".close");
     const navLinks = document.getElementById("nav-links");
+
+    close.style.display = "none";
 
     hamburger.addEventListener("click", function () {
         navLinks.classList.toggle("show");
+
+        if (navLinks.classList.contains("show")) {
+            bars.style.display = "none";
+            close.style.display = "inline";
+        } else {
+            bars.style.display = "inline";
+            close.style.display = "none";
+        }
     });
 });
